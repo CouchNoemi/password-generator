830392d08902bdbbe3a8212ce538975f9cd2243d
NoemiCouch <Noemicouch@gmail.com>
2023-03-13 20:50:06 -0500

    added-content

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # password-generator
+## description
+My motivation to do this project is learning more about javascript, experiment with different codes and improve my skills. I built this project to create a password-generator this can be used to make random password combination. I challenged my imagination and compared the different uses of my code.
+I addeded questions to make the password accesible to everybody on their on conditions
+My project is a web page that includes a box that displays or generates a secure password with a random combination of numbers, special characters, and upper or low characters, also has a specific number of characters.
+
+## screenshot
+![2023-03-13 (3)](https://user-images.githubusercontent.com/116734415/224870682-a305cdaf-6516-42cd-9ebc-589340d38ff2.png)
+## license
+MIT License
+
+Copyright (c) [2023] [Noemi Couch]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+# page-URL
+https://github.com/CouchNoemi/password-generator
--- script.js
+++ script.js
@@ -1,6 +1,88 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +94,47 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  var passwordLength = prompt("Chose a password lenght between 8 and 128");
+  if (!passwordLength) return "No password";
+
+  passwordLength = parseInt(passwordLength); 
+  console.log( typeof passwordLength, passwordLength);
+  if (!passwordLength){
+    alert("please enter a valid number.")
+    return generatePassword();
+  }
+  if(passwordLength < 8 || passwordLength > 128){
+    alert("please enter a number between 8 and 128.")
+    return generatePassword();
+  }
+  var allCharacters = [];
+  var includeNumbers = confirm("Include numbers?");
+  if (includeNumbers){
+    allCharacters = allCharacters.concat(numericCharacters);
+  }
+
+  var allCharacters = [];
+  var includeLowerCasedCharacters = confirm("Include Lower Case?");
+  if (includeLowerCasedCharacters){
+    allCharacters = allCharacters.concat(lowerCasedCharacters);
+  }
+ 
+  var allCharacters = [];
+  var includeUpperCasedCharacters = confirm("Include Upper Case?");
+  if (includeUpperCasedCharacters){
+    allCharacters = allCharacters.concat(upperCasedCharacters);
+  }
+
+  var allCharacters = [];
+  var includeSpecialCharacters = confirm("Include Special Characters?");
+  if (includeSpecialCharacters){
+    allCharacters = allCharacters.concat(specialCharacters);
+  }
+
+  console.log(allCharacters);
+  
+}
+
