dbfe9d4e22912be8a0b05b70aeb6b9bc0d82fdb5
NoemiCouch <Noemicouch@gmail.com>
2023-03-19 16:04:46 -0500

    noemi

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+console.log(password);
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCasedCharacters = [
   'a',
@@ -83,21 +95,11 @@ var specialCharacters = [
   '_',
   '.',
 ];
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-  var passwordLength = prompt("Chose a password lenght between 8 and 128");
+  var passwordLength = prompt("Choose a password length between 8 and 128");
   if (!passwordLength) return "No password";
 
   passwordLength = parseInt(passwordLength); 
@@ -114,27 +116,44 @@ function generatePassword() {
   var includeNumbers = confirm("Include numbers?");
   if (includeNumbers){
     allCharacters = allCharacters.concat(numericCharacters);
+    allCharacters += numericCharacters;
+    console.log(allCharacters);
   }
 
-  var allCharacters = [];
+  //var allCharacters = [];
   var includeLowerCasedCharacters = confirm("Include Lower Case?");
   if (includeLowerCasedCharacters){
     allCharacters = allCharacters.concat(lowerCasedCharacters);
+    allCharacters += lowerCasedCharacters;
+    console.log(allCharacters);
   }
  
-  var allCharacters = [];
+  //var allCharacters = [];
   var includeUpperCasedCharacters = confirm("Include Upper Case?");
   if (includeUpperCasedCharacters){
     allCharacters = allCharacters.concat(upperCasedCharacters);
+    allCharacters += upperCasedCharacters;
+    console.log(allCharacters);
   }
 
-  var allCharacters = [];
+  //var allCharacters = [];
   var includeSpecialCharacters = confirm("Include Special Characters?");
   if (includeSpecialCharacters){
     allCharacters = allCharacters.concat(specialCharacters);
+    allCharacters += specialCharacters;
+    console.log(allCharacters);
   }
 
-  console.log(allCharacters);
-  
+  for(var i=0; i < passwordLength; i++){
+    var randChar = allCharacters[Math.floor(Math.random() * allCharacters.passwordLength)];
+    password += randChar;
+    
+  }
+  console.log(password);
+  return password;
 }
+
+
+  
+
 
